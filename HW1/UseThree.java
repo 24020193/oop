1c9c2db4e2c49de87a900d1fb9a86209cc73a10e
24020193 <24020193@vnu.edu.vn>
2025-09-20 17:20:21 +0700

    xep 3 ten

---
+++ HW1/UseThree.java
@@ -0,0 +1,11 @@
+public class UseThree {
+
+    public static void main(String[] args) {
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+
+        System.out.println("Hi " + name3 + ", " + name2 + ", and " + name1 + ".");
+    }
+
+}
